6c93a1468ed778c1a6e7bb84110b45d94c112c96
Chea David <david.chea@khalibre.com>
2024-12-04 16:51:28 +0700

    Add: intercept & session file

--- cypress/e2e/noncw_user_forgotpassword.cy.js
+++ cypress/e2e/noncw_user_forgotpassword.cy.js
@@ -10,45 +10,45 @@ describe('Non-cw user forgot password', () => {
         beforeEach(() => {
             Cypress.on('uncaught:exception', () => false)
             const url = 'https://uat-connect.firecloud.zone/'
-            const sessionName = 'amy firecloud'
+            // const sessionName = 'amy firecloud'
 
-            cy.session(sessionName, () => {
-                cy.visit(url) // You will be redirect to keycloak page
-                loginToFirecloud (
-                    'firecloud.amy@mail.com',
-                    'Welcome.01',
-                    'Amy Admin'
-                )
-            })
+            // cy.session(sessionName, () => {
+            //     cy.visit(url) // You will be redirect to keycloak page
+            //     loginToFirecloud (
+            //         'firecloud.amy@mail.com',
+            //         'Welcome.01',
+            //         'Amy Admin'
+            //     )
+            // })
             cy.visit(url);
         });
-        it('Login to Firecloud', () => {
-            cy.url().should('include', 'u/home/dashboard')
-        })
+        // it('Login to Firecloud', () => {
+        //     cy.url().should('include', 'u/home/dashboard')
+        // })
 
-        // it('Non-cw user log in to Firecloud', () => {
-        //     cy.get('.authentication-contain').should('be.visible').as('authenticationContain')
+        it('Non-cw user log in to Firecloud', () => {
+            cy.get('.authentication-contain').should('be.visible').as('authenticationContain')
 
-        //     cy.get('@authenticationContain').within( ($content) => {
-        //         cy.wrap($content)
-        //             .should('contain.text', 'Reset password')
-        //             .should('contain.text', 'Don’t worry! Just fill in your information and we’ll send you a link to reset your password.')
+            cy.get('@authenticationContain').within( ($content) => {
+                cy.wrap($content)
+                    .should('contain.text', 'Reset password')
+                    .should('contain.text', 'Don’t worry! Just fill in your information and we’ll send you a link to reset your password.')
 
-        //     })
+            })
             
-        //     cy.get('@authenticationContain').within( () => {
-        //         cy.get('input').focus().blur().type('noaccount@#@@@gmail.com')
-        //         cy.get('.text-danger').contains('Invalid Email Format.')
-        //     })
+            cy.get('@authenticationContain').within( () => {
+                cy.get('input').focus().blur().type('noaccount@#@@@gmail.com')
+                cy.get('.text-danger').contains('Invalid Email Format.')
+            })
 
-        //     cy.get('@authenticationContain').within( () => {
-        //         cy.contains('a.text-black', 'Back to Sign in')
-        //             .should('be.visible')
-        //             .click()
-        //             .then( () => {
-        //                 cy.url().should('include' , 'realms/platform/login-actions')
-        //             })
-        //     })
-        // });
+            cy.get('@authenticationContain').within( () => {
+                cy.contains('a.text-black', 'Back to Sign in')
+                    .should('be.visible')
+                    .click()
+                    .then( () => {
+                        cy.url().should('include' , 'realms/platform/login-actions')
+                    })
+            })
+        });
     });
 });
---
+++ cypress/e2e/session.cy.js
@@ -0,0 +1,30 @@
+const loginToFirecloud = (email, password, expectedProfileName) => {
+    cy.get('#username').type(email)
+    cy.get('#password').type(password)
+    cy.get('#kc-login').click()
+    cy.url().should('include', '/u/home/dashboard')
+}
+
+describe('Non-cw user forgot password', () => {
+    context('Login to Firecloud', () => {
+        beforeEach(() => {
+            Cypress.on('uncaught:exception', () => false)
+            const url = 'https://uat-connect.firecloud.zone/'
+            const sessionName = 'amy firecloud'
+
+            cy.session(sessionName, () => {
+                cy.visit(url) // You will be redirect to keycloak page
+                loginToFirecloud (
+                    'firecloud.amy@mail.com',
+                    'Welcome.01',
+                    'Amy Admin'
+                )
+            })
+            cy.visit(url);
+        });
+        it('Login to Firecloud', () => {
+            cy.url().should('include', 'u/home/dashboard')
+        })
+
+    });
+});
